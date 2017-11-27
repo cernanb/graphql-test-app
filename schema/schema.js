9ebf2c8212cc7078ba92d3885430fd5ce97d0b37
Cernan Bernardo <cernanb@gmail.com>
2017-11-26 21:40:11 -0800

    Add axios to resolve function for users

--- schema/schema.js
+++ schema/schema.js
@@ -1,34 +1,31 @@
-const graphql = require('graphql')
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql
-const _ = require('lodash')
-
-const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id: '27', firstName: 'Samantha', age: 21 }
-]
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const axios = require("axios");
 
 const UserType = new GraphQLObjectType({
-  name: 'User',
+  name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
   }
-})
+});
 
 const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
+  name: "RootQueryType",
   fields: {
     user: {
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id })
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(res => res.data);
       }
     }
   }
-})
+});
 
 module.exports = new GraphQLSchema({
   query: RootQuery
-})
+});
